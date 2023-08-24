c57179165ca4078f0e5e38b24858e8fd8badae6d
Kaushik Karandikar <admin@ktechsolutions.org>
2023-08-24 06:38:46 -0600

    secret key delete

--- src/controllers/amazon.ses.ts
+++ src/controllers/amazon.ses.ts
@@ -1,44 +1,51 @@
 import express from "express";
 import * as AWS from 'aws-sdk';
+import {getAWSSecretKey} from "../helper/get-database-password";
+const secretManager = new AWS.SecretsManager({region: 'us-east-1'});
+const key: any = 'awsSecretKey';
 export const verifyEmail = async (req: express.Request, res: express.Response) => {
 
     try {
-        let params = {
-            Destination: { /* required */
-                CcAddresses: [],
-                ToAddresses: [
-                    'admin@ktechsolutions.org',
-                    /* more items */
-                ]
-            },
-            Message: { /* required */
-                Body: { /* required */
-                    Html: {
-                        Charset: "UTF-8",
-                        Data: "HTML_FORMAT_BODY"
-                    },
-                    Text: {
-                        Charset: "UTF-8",
-                        Data: "TEXT_FORMAT_BODY"
-                    }
-                },
-                Subject: {
-                    Charset: 'UTF-8',
-                    Data: 'http://localhost:4200/subcontractor-questionnaire'
-                }
-            },
-            Source: 'kkarandikar08@gmail.com', /* required */
-
-        };
+        // let params = {
+        //     Destination: { /* required */
+        //         CcAddresses: [],
+        //         ToAddresses: [
+        //             'admin@ktechsolutions.org',
+        //             /* more items */
+        //         ]
+        //     },
+        //     Message: { /* required */
+        //         Body: { /* required */
+        //             Html: {
+        //                 Charset: "UTF-8",
+        //                 Data: "HTML_FORMAT_BODY"
+        //             },
+        //             Text: {
+        //                 Charset: "UTF-8",
+        //                 Data: "TEXT_FORMAT_BODY"
+        //             }
+        //         },
+        //         Subject: {
+        //             Charset: 'UTF-8',
+        //             Data: 'http://localhost:4200/sign-up'
+        //         }
+        //     },
+        //     Source: 'kkarandikar08@gmail.com', /* required */
+        //
+        // };
+        const secretKey = await getAWSSecretKey();
         AWS.config.update({
             region: 'us-east-1',
             apiVersion: "latest"
         })
         let verifyEmail = await new AWS.SES({credentials: {
             accessKeyId: 'AKIAVA5O6P5STZGQMGNW',
-                secretAccessKey: '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'
-            }}).sendEmail(params).promise();
+                secretAccessKey: ''
+            }}).verifyEmailIdentity({EmailAddress: 'admin@ktechsolutions.org'}).promise();
         console.log(verifyEmail.$response);
+        if (verifyEmail.$response.httpResponse.statusCode === 200) {
+            //
+        }
         return res.json(JSON.stringify(verifyEmail.$response.data));
     } catch (e) {
         console.log('entered error : - ', e);
