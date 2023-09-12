09eab358d24e971303a13f5396bbde8f4617a45c
kaushikOtter <kaushik.karandikar@otterproducts.com>
2023-09-12 10:57:15 -0600

    fixed material sources crud

--- src/database/material-sources.ts
+++ src/database/material-sources.ts
@@ -1,6 +1,6 @@
 import {connectToDatabase} from "./database-functions";
 import {MongoClient, ObjectId} from "mongodb";
-import {MaterialAndSources, MaterialSourcesModel} from "../models/material-sources.model";
+import { MaterialSourcesModel} from "../models/material-sources.model";
 
 const DB_NAME = 'joblynk';
 const COLLECTION = 'contractor';
@@ -10,9 +10,9 @@ export const getAllMaterialAndSources = async (contractorId: string) => {
     try {
         client = await connectToDatabase();
         // find the contractor, and then pull the Service Partner array
-        const contractor = await client.db(DB_NAME).collection(COLLECTION).findOne({_id: new ObjectId(contractorId)});
-        console.log('Fetching the contractor object: - ', contractor);
-        return contractor?.materialSources ? contractor.materialSources : null;
+        const materialSources = await client.db(DB_NAME).collection(COLLECTION).find({_id: new ObjectId(contractorId)}).project({materialSources: 1}).toArray();
+        console.log('Fetching the material sources object: - ', materialSources);
+        return materialSources ? materialSources[0]?.materialSources : null;
     } catch (e) {
         console.log(`Error in getAllMaterialAndSources database/material-sources.ts file : - ${e}`);
         throw e;
@@ -21,67 +21,68 @@ export const getAllMaterialAndSources = async (contractorId: string) => {
     }
 }
 
-export const createAdditionalSource = async(matSource: MaterialSourcesModel) => {
+export const createAdditionalSource = async(matSource: MaterialSourcesModel, contractorId: string) => {
 
     let client: MongoClient;
     try {
         client = await connectToDatabase();
         // check if materialSources exists
-        const contractor = await client.db(DB_NAME).collection(COLLECTION).findOne({_id: new ObjectId(matSource.contractorId)});
-        console.log(contractor);
-        // if (!contractor?.materialSources) {
-            return await client.db(DB_NAME).collection(COLLECTION).updateOne({_id: new ObjectId(matSource.contractorId)}, {
-                $set: {
-                    "materialSources": matSource
-                }
-            })
+        matSource._id = new ObjectId().toString();
+        return await client.db(DB_NAME).collection(COLLECTION).updateOne({_id: new ObjectId(contractorId)}, {
+            $addToSet: {
+                "materialSources": matSource
+            }
+        }, {upsert: true});
     } catch (e) {
         console.log(`Error in createSource database/material-sources.ts file : - ${e}`);
         throw e;
     }
 }
 
-export const deleteASource = async(materialSource: MaterialSourcesModel) => {
+
+export const deleteASource = async(contractorId: string, materialSource: MaterialSourcesModel) => {
 
     let client: MongoClient;
     try {
         client = await connectToDatabase();
+        console.log(materialSource);
         // check if materialSources exists
-        const contractor = await client.db(DB_NAME).collection(COLLECTION).findOne({_id: new ObjectId(materialSource.contractorId)});
-        console.log(contractor);
-        // if (!contractor?.materialSources) {
-        return await client.db(DB_NAME).collection(COLLECTION).updateOne({_id: new ObjectId(materialSource.contractorId)}, {
-            $set: {
-                "materialSources": materialSource
+        const result =  await client.db(DB_NAME).collection(COLLECTION).updateOne({_id: new ObjectId(contractorId)}, {
+            $pull: {
+                "materialSources": {
+                        _id: materialSource._id,
+                        material: materialSource.material
+                }
             }
-        })
+        });
+        console.log(result);
+        return result;
     } catch (e) {
         console.log(`Error in deleteSource database/material-sources.ts file : - ${e}`);
         throw e;
     }
 }
 
-export const updateMaterialAndSources = async(source: MaterialAndSources, contractorId: string) => {
+export const updateMaterialAndSources = async(source: MaterialSourcesModel, contractorId: string) => {
 
     let client: MongoClient;
     try {
         console.log('Update MaterialAndSources: - ', source, contractorId);
         client = await connectToDatabase();
         // check if materialSources exists
-        const contractor = await client.db(DB_NAME).collection(COLLECTION).findOne({_id: new ObjectId(contractorId)});
-        console.log('Found the value of the contractor: - ', contractor?.materialSources);
-        if (contractor?.materialSources) {
-            return await client.db(DB_NAME).collection(COLLECTION).updateOne({
-                _id: new ObjectId(contractorId),
-                "materialSources.material.id": source.id
-            }, {
-                $set: {
-                    "materialSources.material.$.sources": source.sources
-                }
-            })
-        } else {
-            throw "No Material and sources present to be updated";
-        }
+        return await client.db(DB_NAME).collection(COLLECTION).updateOne({_id: new ObjectId(contractorId), "materialSources._id": source._id}, {
+            $set: {
+                "materialSources.$.material": source.material,
+                "materialSources.$.primarySource": source.primarySource,
+                "materialSources.$.primarySourceWebsite": source.primarySourceWebsite,
+                "materialSources.$.secondarySource": source.secondarySource,
+                "materialSources.$.secondarySourceWebsite": source.secondarySourceWebsite,
+                "materialSources.$.alternateSource": source.alternateSource,
+                "materialSources.$.alternateSourceWebsite": source.alternateSourceWebsite
+
+            }
+        });
+
     } catch (e) {
         console.log(`Error in updateSource database/material-sources.ts file : - ${e}`);
         throw e;
