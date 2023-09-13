2764bdc22e8cc934a596be0bf9516ae17eba16e2
kaushikOtter <kaushik.karandikar@otterproducts.com>
2023-09-12 23:21:24 -0600

    fixed auth, added send register email to contractors

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -1,10 +1,15 @@
 import express from "express";
 
-import {loginUser, registerUser, verifyValidToken} from "../controllers/authentication";
+import {
+    loginUser,
+    registerUser,
+    sendRegistrationEmailToContractor, verifyValidToken
+} from "../controllers/authentication";
 import {verifyToken} from "../middleware/auth-verify";
 
 export default (router: express.Router) => {
     router.post('/auth/register', registerUser);
     router.post('/auth/login', loginUser);
-    router.post('/auth/verify', verifyToken, verifyValidToken);
+    router.post('/auth/verify', verifyValidToken);
+    router.post('/auth/contractor/registerEmail', sendRegistrationEmailToContractor);
 }
